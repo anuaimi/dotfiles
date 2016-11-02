c42cd93a1d09a59b8e9bf885490a7f05661abe36
Athir Nuaimi <athir@nuaimi.com>
2016-11-01 23:41:02 -0400

    add gitconfig,wgetrc files

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ PWD=$(pwd)
 #Linking ~/.bashrc -> /Users/athirnuaimi/projects/dotfiles/bashrc
 ln -s $PWD/aliases ~/.aliases
 ln -s $PWD/curlrc ~/.curlrc
+ln -s $PWD/wgetrc ~/.wgetrc
 ln -s $PWD/gemrc ~/.gemrc
-#Linking ~/.gitconfig -> /Users/athirnuaimi/projects/dotfiles/gitconfig
-#Linking ~/.gitignore -> /Users/athirnuaimi/projects/dotfiles/gitignore
+ln -s $PWD/gitconfig ~/.gitconfig
 #Linking ~/.hgrc -> /Users/athirnuaimi/projects/dotfiles/hgrc
 #Linking ~/.irbrc -> /Users/athirnuaimi/projects/dotfiles/irbrc
 #Linking ~/.irssi -> /Users/athirnuaimi/projects/dotfiles/irssi
